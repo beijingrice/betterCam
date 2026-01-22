f3e18ca4386d229afdbf0dc831f00d8a4152eddc
beijingrice <13521019795@163.com>
2026-01-22 14:00:37 +0800

    Initial Commit

---
+++ betterCam/betterCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  betterCamApp.swift
+//  betterCam
+//
+//  Created by Rice on 2026/1/22.
+//
+
+import SwiftUI
+
+@main
+struct betterCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ betterCamTests/betterCamTests.swift
@@ -0,0 +1,17 @@
+//
+//  betterCamTests.swift
+//  betterCamTests
+//
+//  Created by Rice on 2026/1/22.
+//
+
+import Testing
+@testable import betterCam
+
+struct betterCamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
